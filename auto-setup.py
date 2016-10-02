cee39579b74aab3e9b0448ca6b7e41f519ac300e
moloch <moloch38@gmail.com>
2016-10-01 14:57:10 -1000

    Added more features to auto-setup.py

--- auto-setup.py
+++ auto-setup.py
@@ -53,53 +53,91 @@ def nginx_conf(compose, is_prod):
     print "(ex. localhost, www.example.com)"
     hostname = raw_input(PROMPT + "Root domain? ")
     if hostname != "":
-        compose["domain"] = hostname
-    return
+        compose["services"]["api"]["environment"].append(
+            "XSSHUNTER_DOMAIN=%s" % hostname
+        )
 
 
 def email_conf(compose, is_prod):
-    environ = compose["services"]["api"]["environment"]
     print "Great! Now let's setup your Mailgun account to send XSS alerts to."
     print "We'll need yo API key (ex. key-8da843ff65205a61374b09b81ed0fa35)"
-    environ["mailgun_api_key"] = raw_input(PROMPT + "API key: ")
+    mailgun_api_key = raw_input(PROMPT + "API key: ")
     print ""
+    compose["services"]["api"]["environment"].append(
+        "XSSHUNTER_MAILGUN_API_KEY=%s" % mailgun_api_key
+    )
 
     print "What is your Mailgun domain? (ex. example.com)"
-    environ["mailgun_sending_domain"] = raw_input(PROMPT + "Mailgun domain: ")
+    mailgun_sending_domain = raw_input(PROMPT + "Mailgun domain: ")
     print ""
+    compose["services"]["api"]["environment"].append(
+        "XSSHUNTER_MAILGUN_SENDING_DOMAIN=%s" % mailgun_sending_domain
+    )
 
     print "What email address is sending the payload fire emails?: "
     print "(ex. no-reply@example.com)"
-    environ["email_from"] = raw_input(PROMPT + "Sending email address: ")
+    email_from = raw_input(PROMPT + "Sending email address: ")
     print ""
+    compose["services"]["api"]["environment"].append(
+        "XSSHUNTER_EMAIL_FROM=%s" % email_from
+    )
 
-    print "Where should abuse/contact emails go?: "
+    print "Where should abuse/contact emails go? "
     print "(ex. yourpersonal@gmail.com)"
-    environ["abuse_email"] = raw_input(PROMPT + "Abuse/Contact email: ")
+    email_abuse = raw_input(PROMPT + "Abuse/Contact email: ")
     print ""
+    compose["services"]["api"]["environment"].append(
+        "XSSHUNTER_EMAIL_ABUSE=%s" % email_abuse
+    )
 
 
 def database_conf(compose, is_prod):
-    print "What postgres user is this service using? "
-    print "(ex. xsshunter)"
-    compose["postgreql_username"] = raw_input(PROMPT + "Postgres username: ")
+    """ Sets up the Postgres database """
+    print "What Postgres username? (ex. xsshunter)"
+    db_username = raw_input(PROMPT + "Postgres username: ")
     print ""
+    compose["services"]["sql"]["environment"].append(
+        "POSTGRES_USER=%s" % db_username
+    )
+    compose["services"]["api"]["environment"].append(
+        "XSSHUNTER_SQL_USER=%s" % db_username
+    )
 
-    print "What is the postgres user's password? (ex. @!$%@^%UOFGJOEJG$)"
-    compose["postgreql_password"] = raw_input(PROMPT + "Postgres password: ")
+    print "What is the Postgres user's password? (ex. @!$%@^%UOFGJOEJG$)"
+    db_password = raw_input(PROMPT + "Postgres password: ")
     print ""
+    compose["services"]["sql"]["environment"].append(
+        "POSTGRES_PASSWORD=%s" % db_password
+    )
+    compose["services"]["api"]["environment"].append(
+        "XSSHUNTER_SQL_PASSWORD=%s" % db_password
+    )
 
-    print "What is the postgres user's DB? (ex. xsshunter)"
-    compose["postgres_db"] = raw_input(PROMPT + "Postgres DB: ")
+    print "What is the name of the Postgres database? (ex. xsshunter)"
+    db_name = raw_input(PROMPT + "Postgres DB: ")
     print ""
+    compose["services"]["sql"]["environment"].append(
+        "POSTGRES_DB=%s" % db_name
+    )
+    compose["services"]["api"]["environment"].append(
+        "XSSHUNTER_SQL_DATABASE=%s" % db_name
+    )
+
+    if not is_prod:
+        print WARN + "Exposing database port 5432 for debugging"
+        compose["services"]["sql"]["ports"] = ['"5432:5432"']
+    return db_username, db_password, db_name
 
 
 def secrets_conf(compose, is_prod):
     print INFO + " Generating cookie secret..."
-    compose["cookie_secret"] = os.urandom(32).encode('hex')
+    secret = os.urandom(32).encode('hex')
+    compose["services"]["api"]["environment"].append(
+        "XSSHUNTER_COOKIE_SECRET=%s" % secret
+    )
 
 
-def print_footer(hostname):
+def print_footer():
     print """
     Also, please ensure your wildcard SSL certificate and key are available at
     the following locations:
@@ -126,6 +164,7 @@ def main():
                 'environment': [
                     "XSSHUNTER_LISTEN_PORT=8888",
                 ],
+                'expose': ["8888"],
                 'volumes': [],
                 'depends_on': ['sql']
             },
@@ -150,40 +189,41 @@ def main():
         print WARN + "%sDevelopement deployment%s, debug features enabled!" % (
             BOLD, W
         )
+        compose["services"]["api"]["environment"].append("XSSHUNTER_DEBUG=1")
     print ""
 
     # Docker base images
     if yes_no_prompt("Should I build the docker base images for you"):
         build_docker_base()
 
-    # Email setup
-    if yes_no_prompt("Should I setup email support"):
-        email_conf(compose)
-
     # Database setup
     if yes_no_prompt("Should I setup the database now"):
-        database_conf(compose)
+        database_conf(compose, is_prod)
+
+    # Email setup
+    if yes_no_prompt("Should I setup email support"):
+        email_conf(compose, is_prod)
 
     if yes_no_prompt("Should I configure Nginx for you"):
         print INFO + "Minting new nginx configuration file ..."
-        file_handler = open("default", "w")
-        conf = nginx_conf()
-        file_handler.write(conf)
-        file_handler.close()
+        nginx_conf(compose, is_prod)
 
+    yaml_config = yaml.dump(compose, default_flow_style=False)
+    print "-" * 60
+    for index, line in enumerate(yaml_config.split('\n')):
+        print "%02d. %s" % (index + 1, line)
+    print "\n", "-" * 60
     if yes_no_prompt("Should I save all of the current settings"):
-        yaml_config = yaml.dump(compose, default_flow_style=False)
-        file_handler = open(DOCKERCOMPOSEFILE, "w")
-        file_handler.write(yaml_config)
-        file_handler.close()
+        with open(DOCKERCOMPOSEFILE, "w") as fp:
+            fp.write(yaml_config)
 
     print_footer()
     if yes_no_prompt("Should I fire up the whole stack for you now"):
         os.system("docker-compose up -f %s" % DOCKERCOMPOSEFILE)
     else:
         print ""
-        print INFO + "Okay no worries, you can start it yourself with: "
-        print "docker-compose up -f %s" % DOCKERCOMPOSEFILE
+        print INFO + "Okay no worries, you can start it yourself with:\n"
+        print "docker-compose up\n"
 
 
 if __name__ == "__main__":
